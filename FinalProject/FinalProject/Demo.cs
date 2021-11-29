0820c514fa73c4512a4a98cb0718fb6f3cbe9cdc
Brandon Perez <Bloo7885@hotmail.com>
2021-11-29 14:57:13 -0500

    demo

---
+++ FinalProject/FinalProject/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MingweiSamuel.Camille;
+using MingweiSamuel.Camille.Enums;
+using MingweiSamuel.Camille.SummonerV4;
+
+namespace FinalProject
+{
+    class Demo
+    {
+        static async Task Demo(string[] args)
+        {
+            var riotApi = new RiotApi("RGAPI-letters1-letters2-letters3-letters4-letters5");
+
+            var summoner = riotApi.SummonerV4.GetSummonerByName("Kingbran21");
+            Console.WriteLine($"Name: {summoner.Name} Level: {summoner.SummonerLevel}");
+
+
+        }
+    }
+}
---
+++ FinalProject/FinalProject/SummonerV4.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FinalProject
+{
+    public class SummonerV4
+    {
+        private static readonly Uri ApiBaseAddress;
+        private RiotApi Api { get; }
+
+        public SummonerV4(RiotApi api)
+        {
+            Api = api;
+        }
+
+        public Summoner GetSummonerByName(string summonerName)
+        {
+            var uriBuilder = new UriBuilder($"{ApiBaseAddress}/by-name/{summonerName}");
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["api_key"] = Api.ApiKey;
+            uriBuilder.Query = query.ToString();
+            string requestUri = uriBuilder.ToString();
+
+            var response = Api.Client.GetAsync(requestUri).Result;
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                throw new Exception($"Request failed. Response: {response.StatusCode}");
+            }
+
+            string jsonResponse = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<Summoner>(jsonResponse);
+
+        }
+
+        public async Task<Summoner> GetSummonerByNameAsync(string summonerName)
+    }
+}
