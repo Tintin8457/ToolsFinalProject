34b19629a34e73d7df64ba2f425e198b7c84d1a3
Brandon Perez <Bloo7885@hotmail.com>
2021-12-01 14:53:01 -0500

    up

--- FinalProject/FinalProject/Demo.cs
+++ FinalProject/FinalProject/Demo.cs
@@ -10,7 +10,7 @@ namespace FinalProject
     {
         static async Task Demo(string[] args)
         {
-            var riotApi = new RiotApi("RGAPI-letters1-letters2-letters3-letters4-letters5");
+            var riotApi = new RiotApi("RGAPI-6e223d95-5d4f-41da-916a-fa458f392fa6");
 
             var summoner = riotApi.SummonerV4.GetSummonerByName("Kingbran21");
             Console.WriteLine($"Name: {summoner.Name} Level: {summoner.SummonerLevel}");
